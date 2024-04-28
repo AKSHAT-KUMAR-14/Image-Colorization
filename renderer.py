320fb909eaa8f9992609ce7a47f8010499607542
Akshat Kumar <akshatkumar0110@gmail.com>
2024-04-29 00:36:45 +0530

    hel

--- renderer.py
+++ renderer.py
@@ -29,12 +29,11 @@ def colorize():
     
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        filepath = os.path.join('uploads', filename)
+        filepath = os.path.join('static', filename)
         file.save(filepath)
         
         # Load colorizers
         colorizer_eccv16 = eccv16(pretrained=True).eval()
-        colorizer_siggraph17 = siggraph17(pretrained=True).eval()
         
         # Preprocess image
         img = load_img(filepath)
@@ -42,21 +41,15 @@ def colorize():
         
         # Colorize image using ECCV16 and SIGGRAPH17 models
         out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
-        out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
         
         # Save colorized images
-        filename_eccv16 = f'eccv16_{filename}'
-        filepath_eccv16 = os.path.join('uploads', filename_eccv16)
+        filename_eccv16 = 'Colorized_img.jpg'
+        filepath_eccv16 = os.path.join('static', filename_eccv16)
         plt.imsave(filepath_eccv16, out_img_eccv16)
-        
-        filename_siggraph17 = f'siggraph17_{filename}'
-        filepath_siggraph17 = os.path.join('uploads', filename_siggraph17)
-        plt.imsave(filepath_siggraph17, out_img_siggraph17)
         
         # Pass file paths to the template
         return render_template('index.html', 
-                               colored_image_eccv16=filepath_eccv16, 
-                               colored_image_siggraph17=filepath_siggraph17)
+                               colored_image_eccv16=filepath_eccv16)
 
     return jsonify({'error': 'Invalid file format'}), 400
 
